f057733c48fca9cf31f030a2f5dc24b6841f3e3a
LeiWithP <pedro.barrita1029@gmail.com>
2023-06-01 09:06:13 -0600

    Excercise 3 V0.1

--- logic.pl
+++ logic.pl
@@ -1,8 +1,52 @@
-:- dynamic(father/2).
 
-father(michael, john).
-father(michael, gina).
-father(john, sara).
+piso(madera).
+piso(ceramica).
+piso(parquet).
+piso(laminado).
+piso(piedra).
+piso(alfombra).
+piso(marmol).
+piso(granito).
+piso(azulejo).
+piso(baldosa).
 
-% Query and print the results
-% father(michael, X).
+ventana(corrediza).
+ventana(abatible).
+ventana(guillotina).
+ventana(batiente).
+ventana(proyectante).
+ventana(fija).
+ventana(plegable).
+ventana(oscilobatiente).
+ventana(panoramica).
+ventana(laminada).
+
+elementos(P,V):-piso(P), ventana(V).
+
+costo(madera, 5000).
+costo(ceramica, 7000).
+costo(parquet, 10000).
+costo(laminado, 8000).
+costo(piedra, 12000).
+costo(alfombra, 6000).
+costo(marmol, 15000).
+costo(granito, 9000).
+costo(azulejo, 7500).
+costo(baldosa, 8500).
+
+costo(corrediza, 2000).
+costo(abatible, 2500).
+costo(guillotina, 3000).
+costo(batiente, 2200).
+costo(proyectante, 2800).
+costo(fija, 1800).
+costo(plegable, 3200).
+costo(oscilobatiente, 2700).
+costo(panoramica, 4000).
+costo(laminada, 3500).
+
+combinaciones(P,V, Presupuesto, Total):-
+        elementos(P,V), 
+        costo(P,CP), costo(V,CV), 
+        Total is CP + CV,
+        Total =< Presupuesto.
--- main.py
+++ main.py
@@ -3,20 +3,14 @@ from fastapi.responses import JSONResponse
 from pyswip import Prolog
 
 app = FastAPI()
-
+prolog = Prolog()
+prolog.consult("logic.pl")
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/prolog")
-def polog_endpoint():
-    results = []
-    prolog = Prolog()
-    prolog.consult("logic.pl")
-    
-    results = list(prolog.query("father(X,Y)"))
-    for soln in prolog.query("father(X,Y)"):
-        print(soln["X"], "is the father of", soln["Y"])
-        #results.append(soln["X"] + " is the father of " + soln["Y"])
+@app.get("/prolog/{query}")
+async def prolog_endpoint(query):
+    results = list(prolog.query(query))
     return results
